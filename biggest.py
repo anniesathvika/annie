cff957880e03c71a1955404dd40eab406ea5d9ad
anniesathvika <99342480+anniesathvika@users.noreply.github.com>
2022-02-18 11:26:19 +0530

    Create biggest.py

---
+++ biggest.py
@@ -0,0 +1,12 @@
+a=int(input('enter a'))
+b=int(input('enter b'))
+c=int(input('enter c'))
+if a>b:
+if a>c:
+print('a is big')
+if b>c:
+if b>a:
+print('b is big')
+if c>a:
+if c>b:
+print('c is big')
